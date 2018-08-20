5c957c4a5f74aa439a14fb2d027312faa34e0939
supper1 <ou66ou88@hotmail.com>
2018-08-20 16:47:20 +0800

    引入less，并修改部分样式

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,46 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <router-link to="/">
+    <div class="back"><Icon type="md-arrow-round-back" /></div>
+    </router-link>
   </div>
 </template>
+
+<script>
+
+export default {
+  name: 'home',
+}
+</script>
+
+<style lang="less" scoped>
+  @fcolor : #285aa5;
+  @bgcolor :rgba(255, 255, 255, 0.7);
+  @bgborder : rgba(255, 255, 255, 0.1);
+  .about{
+    width: 800px;
+    height: 600px;
+    margin: 100px auto;
+    background-color: @bgcolor;
+    border: 30px solid @bgborder;
+    box-sizing: border-box;
+    a{
+      color: #333;
+    }
+    .back{
+      margin: 5px 10px;
+      width: 50px;
+      height: 50px;
+      font-size: 30px;
+      text-align: center;
+      line-height: 50px;
+      border-radius: 8px;
+      transition: all .3s linear;
+    }
+    .back:hover{
+      background-color: rgba(255, 255, 255, 0.4);
+      color:@fcolor;
+    }
+
+  }
+</style>
