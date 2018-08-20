4406fda69e05df1e76407ed6e4e98cba81bbacd1
supper1 <ou66ou88@hotmail.com>
2018-08-20 14:31:51 +0800

    引入iview

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,29 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    
     <router-view/>
   </div>
 </template>
+<script>
+    export default {
+      
+    };
+    let a = true;
+    setInterval(() => {
+      a = !a;
+      if (a) {
+        document.body.style.backgroundPosition = "center -20px";
+      } else {
+        document.body.style.backgroundPosition = "center 0px";
+      }
+    }, 1000);
+</script>
+
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+    body {
+      background: url(../public/bg.jpg) center 0 no-repeat;
+      background-size: 1920px 1080px;
+      transition: background 1s linear;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
-import App from './App.vue'
+
 import router from './router'
 import store from './store'
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
+
+import App from './App.vue'
+Vue.use(iView);
 
 Vue.config.productionTip = false
 
